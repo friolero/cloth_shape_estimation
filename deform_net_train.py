0d28673ce3e22d7442c69e812ce55a5daeb4fade
Zeng Yuwei <15153187+friolero@users.noreply.github.com>
2023-04-08 11:33:44 +0800

    added logging and checkpoint saving

--- deform_net_train.py
+++ deform_net_train.py
@@ -1,6 +1,10 @@
+import os
+import sys
+
 import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
 from data_utils import get_adjacency_matrix, load_wavefront_file
@@ -9,6 +13,16 @@ from deform_net import DeformNet
 from differentiable_rendering import CameraInterface, init_lighting
 
 if __name__ == "__main__":
+
+    exp_name = sys.argv[1]
+    vis_dir = f"vis/{exp_name}"
+    output_dir = f"output/{exp_name}"
+    log_dir = f"log/{exp_name}"
+    if not os.path.isdir(vis_dir):
+        os.system(f"mkdir -p {vis_dir}")
+    if not os.path.isdir(output_dir):
+        os.system(f"mkdir -p {output_dir}")
+    writer = SummaryWriter(log_dir)
 
     if torch.cuda.is_available():
         device = torch.device("cuda:0")
@@ -51,12 +65,17 @@ if __name__ == "__main__":
     )
 
     # initialize model, criterion, lr scheduler and optimizer
+    lr = 1e-4
+    betas = (0.9, 0.999)
+    lr_scheduler_patience = 5
     model = DeformNet(cano_verts).to(device)
-    optimizer = optim.Adam(model.parameters(), lr=1e-4, betas=(0.9, 0.999))
+    optimizer = optim.Adam(model.parameters(),
+                           lr=lr,
+                           betas=betas)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(
         optimizer,
         mode="min",
-        patience=5,
+        patience=lr_scheduler_patience,
     )
     w_rgb = 0.0
     w_depth = 0  # 0.1
@@ -64,15 +83,31 @@ if __name__ == "__main__":
     w_laplacian = 0  # 0
     w_offset = 1  # 0
 
-    vis = False
-    n_print = 100
-    for epoch_idx in range(n_epoch):
 
+    vis = False
+    best_loss = 1e9
+    n_log = 1
+    writer.add_hparams({'lr': lr,
+                        'betas': torch.Tensor(betas),
+                        'lr_scheduler_patience': lr_scheduler_patience,
+                        'w_rgb': w_rgb,
+                        'w_depth': w_depth,
+                        'w_normals': w_normals,
+                        'w_offset': w_offset,
+                        'w_laplacian': w_laplacian}, dict())
+    for epoch_idx in range(n_epoch):
         print(f"====================Epoch {epoch_idx}====================")
         # training
         n_data = 0
-        total_loss = 0
+        total_loss = {'weighted': 0,
+                      'rgb': 0,
+                      'depth': 0,
+                      'normals': 0,
+                      'offset': 0,
+                      'laplacian': 0}
         for idx, batch in enumerate(tqdm(train_dl)):
+            #if idx >= 10:
+            #    break
 
             model.train()
             optimizer.zero_grad()
@@ -86,6 +121,25 @@ if __name__ == "__main__":
             # differentiable rendering the predicted mesh
             dfm_mesh = cano_mesh.offset_verts(pred_offsets.reshape(-1, 3))
             tgt_render = camera.render(dfm_mesh)
+            if idx == 0:
+                with torch.no_grad():
+                    gt_mesh = cano_mesh.offset_verts(offsets.reshape(-1, 3))
+                    gt_render = camera.render(gt_mesh)
+                vis_images = []
+                for mode in camera.mode:
+                    pred = torch.cat([img.permute([2, 0, 1]) for img in tgt_render[mode][..., :3]], 2)
+                    gt = torch.cat([img.permute([2, 0, 1]) for img in gt_render[mode][..., :3]], 2)
+                    vis_img = torch.cat([gt, pred], 1)
+                    if mode == 'depth':
+                        vis_img = (vis_img - vis_img.min()) / (vis_img.max() - vis_img.min())
+                        vis_img = vis_img.repeat([3, 1, 1])
+                    if mode == 'normals':
+                        vis_img = ((vis_img + 1.0) / 2)[[0, 2, 1]]
+                        vis_mask = ~vis_img.eq(torch.Tensor([0.5, 0.5, 0.5]).reshape(-1, 1, 1).to(vis_img.device))
+                        vis_img *= vis_mask
+                    vis_images.append(vis_img)
+                writer.add_image(f"train/images", torch.cat(vis_images, 1), epoch_idx)
+                del gt_mesh, gt_render, vis_images, vis_img
             pred_rgb = tgt_render["rgb"][..., :3].permute([0, 3, 1, 2])
             pred_depth = (
                 tgt_render["depth"].permute([0, 3, 1, 2]).repeat([1, 3, 1, 1])
@@ -114,34 +168,53 @@ if __name__ == "__main__":
                 + w_offset * offset_loss
                 + w_laplacian * laplacian_loss
             )
-            total_loss += loss.item() * offsets.shape[0]
-            n_data += offsets.shape[0]
-
             # backwarding loss
             loss.backward()
             optimizer.step()
 
-            if idx % n_print == 0:
-                print("  mean_train_loss:", total_loss / n_data)
-                print("      (rgb_loss):", rgb_loss.item())
-                print("      (depth_loss):", depth_loss.item())
-                print("      (normals_loss):", normals_loss.item())
-                print("      (offset_loss):", offset_loss.item())
-                print("      (laplacian_loss):", laplacian_loss.item())
-        print("Train_loss:", total_loss / n_data)
+            n_batch_data = offsets.shape[0]
+            total_loss['weighted'] += loss.item() * n_batch_data
+            total_loss['rgb'] += rgb_loss.item() * n_batch_data
+            total_loss['depth'] += depth_loss.item() * n_batch_data
+            total_loss['normals'] += normals_loss.item() * n_batch_data
+            total_loss['offset'] += offset_loss.item() * n_batch_data
+            total_loss['laplacian'] += laplacian_loss.item() * n_batch_data
+            n_data += n_batch_data
+
+            if idx % n_log == 0:
+                step = len(train_dl) * epoch_idx + idx
+                writer.add_scalar('train/weighted_loss', loss, step)
+                writer.add_scalar('train/rgb_loss', rgb_loss.item(), step)
+                writer.add_scalar('train/depth_loss', depth_loss.item(), step)
+                writer.add_scalar('train/normals_loss', normals_loss.item(), step)
+                writer.add_scalar('train/offset_loss', offset_loss.item(), step)
+                writer.add_scalar('train/laplacian_loss', laplacian_loss.item(), step)
+        for k, v in total_loss.items():
+            writer.add_scalar(f'loss/{k}/train', v / n_data, epoch_idx)
 
         n_data = 0
-        total_loss = 0
+        total_loss = {'weighted': 0,
+                      'rgb': 0,
+                      'depth': 0,
+                      'normals': 0,
+                      'offset': 0,
+                      'laplacian': 0}
         for idx, batch in enumerate(tqdm(eval_dl)):
+            #if idx >= 10:
+            #    break
+
             model.eval()
             with torch.no_grad():
                 # model forwarding
                 batch = [data.to(device) for data in batch]
                 rgb, depth, normals, scale, offsets = batch
                 mask = depth > 0
-                pred_dfm_vtx, pred_offsets, _ = model(
-                    scale, rgb, depth, normals
-                )
+                try:
+                    pred_dfm_vtx, pred_offsets, _ = model(
+                        scale, rgb, depth, normals
+                    )
+                except:
+                    import ipdb; ipdb.set_trace()
 
                 # differentiable rendering the predicted mesh
                 dfm_mesh = cano_mesh.offset_verts(pred_offsets.reshape(-1, 3))
@@ -157,7 +230,21 @@ if __name__ == "__main__":
                         vis=vis,
                         save_prefix=f"vis/eval_gt_e{epoch_idx}",
                     )
-                    del gt_mesh
+                    vis_images = []
+                    for mode in camera.mode:
+                        pred = torch.cat([img.permute([2, 0, 1]) for img in tgt_render[mode][..., :3]], 2)
+                        gt = torch.cat([img.permute([2, 0, 1]) for img in gt_render[mode][..., :3]], 2)
+                        vis_img = torch.cat([gt, pred], 1)
+                        if mode == 'depth':
+                            vis_img = (vis_img - vis_img.min()) / (vis_img.max() - vis_img.min())
+                            vis_img = vis_img.repeat([3, 1, 1])
+                        if mode == 'normals':
+                            vis_img = ((vis_img + 1.0) / 2)[[0, 2, 1]]
+                            vis_mask = ~vis_img.eq(torch.Tensor([0.5, 0.5, 0.5]).reshape(-1, 1, 1).to(vis_img.device))
+                            vis_img *= vis_mask
+                        vis_images.append(vis_img)
+                    writer.add_image(f"eval/images", torch.cat(vis_images, 1), epoch_idx)
+                    del gt_mesh, gt_render, vis_images, vis_img
                 else:
                     tgt_render = camera.render(dfm_mesh)
                 pred_rgb = tgt_render["rgb"][..., :3].permute([0, 3, 1, 2])
@@ -192,16 +279,27 @@ if __name__ == "__main__":
                     + w_offset * offset_loss
                     + w_laplacian * laplacian_loss
                 )
-                total_loss += loss.item() * offsets.shape[0]
-                n_data += offsets.shape[0]
                 scheduler.step(loss)
+                n_batch_data = offsets.shape[0]
+                total_loss['weighted'] += loss.item() * n_batch_data
+                total_loss['rgb'] += rgb_loss.item() * n_batch_data
+                total_loss['depth'] += depth_loss.item() * n_batch_data
+                total_loss['normals'] += normals_loss.item() * n_batch_data
+                total_loss['offset'] += offset_loss.item() * n_batch_data
+                total_loss['laplacian'] += laplacian_loss.item() * n_batch_data
+                n_data += n_batch_data
 
-            if idx % n_print == 0:
-                print(f"Step {idx}:")
-                print("  mean_eval_loss:", total_loss / n_data)
-                print("      (rgb_loss):", rgb_loss.item())
-                print("      (depth_loss):", depth_loss.item())
-                print("      (normals_loss):", normals_loss.item())
-                print("      (offset_loss):", offset_loss.item())
-                print("      (laplacian_loss):", laplacian_loss.item())
-        print("Eval_loss:", total_loss / n_data)
+        eval_loss = total_loss['weighted'] / n_data
+        for k, v in total_loss.items():
+            writer.add_scalar(f'loss/{k}/eval', v / n_data, epoch_idx)
+        if best_loss > eval_loss:
+            ckpt_fn = f"{output_dir}/model_e{epoch_idx}_{eval_loss:.4f}.pt"
+            torch.save(
+                {
+                    'epoch': epoch_idx,
+                    'loss': eval_loss,
+                    'optimizer': optimizer.state_dict(),
+                    'model': model.state_dict(),
+                }, ckpt_fn)
+            best_loss = eval_loss
+            print(f"Saving the model to {ckpt_fn}")
